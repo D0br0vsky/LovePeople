dd753a0c4c52561504d8815095415c7730f05d4d
Dobrovsky <miron.dobrovsky.work@gmail.com>
2024-07-14 13:28:15 +0300

    Initial Commit

---
+++ LovePeople/LovePeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LovePeopleApp.swift
+//  LovePeople
+//
+//  Created by Dobrovsky on 14.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct LovePeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
